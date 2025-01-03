472c92edeb3bad2249eb4934d0a259358c4b2657
Mac mini <tigrastov@yandex.ru>
2025-01-03 13:30:30 +0100

    Empty project

---
+++ Foody/FoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodyApp.swift
+//  Foody
+//
+//  Created by Mac mini on 03.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
